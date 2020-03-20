119bbcd7d7c90c5e5364e7583cf2909a5ec5cfa9
cho jae kwon <jjg2362@naver.com>
2020-03-20 19:56:21 +0900

    first commit

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LandingPage() {
+   return (
+      <>
+         <div className="app">
+            <span style={{ fontSize: "2rem" }}>Welcome to visit DNA Homepage!!!</span>
+         </div>
+      </>
+   );
+}
+
+export default LandingPage;
