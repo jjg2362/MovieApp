646a1de83ca481dafaaaea3ff3cb9f7feeececef
cho jae kwon <jjg2362@naver.com>
2020-03-23 21:16:48 +0900

    add landgin page and banner

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,5 @@
 export const USER_SERVER = "/api/users";
 
 export const API_URL = "https://api.themoviedb.org/3/";
-
+export const API_KEY = "eb0a38ed2aef1fe4f3d616ce3b977b19";
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
+import Banner from "./Section/Banner";
 
 function LandingPage() {
+   const [Movies, setMovies] = useState([]);
+   const [BannerMovie, setBannerMovie] = useState(null);
+
+   useEffect(() => {
+      const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+      fetch(endpoint)
+         .then(response => response.json())
+         .then(response => {
+            setMovies([...response.results]);
+            setBannerMovie(response.results[0]);
+         });
+   }, []);
+
    return (
-      <>
-         <div className="app">
-            <span style={{ fontSize: "2rem" }}>Welcome to visit DNA Homepage!!!</span>
+      BannerMovie !== null && (
+         <div style={{ width: "100%", margin: "0" }}>
+            {/* Banner */}
+            <Banner image={`${IMAGE_BASE_URL}w1280${BannerMovie.backdrop_path}`} title={BannerMovie.title} desc={BannerMovie.overview} />
+
+            <div style={{ width: "85%", margin: "1rem auto" }}>
+               <h2>Movies by latest</h2>
+               <hr />
+            </div>
+
+            <div style={{ display: "flex", justifyContent: "center" }}>
+               <button>Load more</button>
+            </div>
          </div>
-      </>
+      )
    );
 }
 
