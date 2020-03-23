89576dbe12ae503100079de6be6b5dd0c9e29d52
cho jae kwon <jjg2362@naver.com>
2020-03-23 22:29:24 +0900

    add load more button

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from "react";
+import { Row } from "antd";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
 import Banner from "./Section/Banner";
+import GridCard from "../common/GridCard";
 
 function LandingPage() {
    const [Movies, setMovies] = useState([]);
    const [BannerMovie, setBannerMovie] = useState(null);
+   const [CurrentPage, setCurrentPage] = useState(0);
 
    useEffect(() => {
       const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+      fetchMovies(endpoint);
+   }, []);
+
+   const fetchMovies = endpoint => {
       fetch(endpoint)
          .then(response => response.json())
          .then(response => {
-            setMovies([...response.results]);
+            setMovies([...Movies, ...response.results]);
             setBannerMovie(response.results[0]);
+            setCurrentPage(response.page);
          });
-   }, []);
+   };
+
+   const onButton = () => {
+      const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
+      fetchMovies(endpoint);
+   };
 
    return (
       BannerMovie !== null && (
@@ -25,10 +38,17 @@ function LandingPage() {
             <div style={{ width: "85%", margin: "1rem auto" }}>
                <h2>Movies by latest</h2>
                <hr />
-            </div>
 
+               <Row gutter={[16, 16]}>
+                  {Movies.map(movie => (
+                     <React.Fragment key={movie.id}>
+                        <GridCard image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}` : null} movieName={movie.title} movieId={movie.id}></GridCard>
+                     </React.Fragment>
+                  ))}
+               </Row>
+            </div>
             <div style={{ display: "flex", justifyContent: "center" }}>
-               <button>Load more</button>
+               <button onClick={onButton}>Load more</button>
             </div>
          </div>
       )
